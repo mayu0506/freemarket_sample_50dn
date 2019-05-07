9f44822d568ee356dcdd737191c4c34bdf299513
pon182 <pxnxt_182@hotmail.com>
2019-05-07 18:58:42 +0900

    Revert "Revert "Merge pull request #19 from mayu0506/add-basic-systems""
    This reverts commit b115cf46a9d11ce9fb1781c1b1a841681a60b477.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
+  private
+  # Basic認証を本番環境のみで有効にするための記述です。
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
