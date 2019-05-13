ef509514725e77c4a3a2062399531613695c36a2
pon182 <pxnxt_182@hotmail.com>
2019-05-13 12:23:53 +0900

    conflict　routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#show'
-  resources :products, only: [:index, :new, :show]
+  root 'products#index'
+  resources :products, only: [:index, :new, :show] do
+    get :buy, on: :member
+  end
+    # 実装時showのみに変更　productsに入れ子
   resources :categorys, only: [:index,:show]
-
   resources :users do
     get :logout, on: :collection
+    get :identification, on: :collection
   end
+  resources :payments, only: [:index, :new]
 end
