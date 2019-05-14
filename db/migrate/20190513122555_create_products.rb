be64a34f8be991f9f13576a1059cdf29a5fd5259
marikattt <mtsuccesfmg.96@gmail.com>
2019-05-14 11:42:25 +0900

    Edit.create-product-model

---
+++ db/migrate/20190513122555_create_products.rb
@@ -0,0 +1,19 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name,                                    null: false
+      t.text :description,                               null: false
+      t.integer :price,                                  null: false 
+      t.string :condition,                               null: false
+      t.string :status,                                  null: false, default: 'selling'
+      t.string :who_to_pay,                              null: false
+      t.string :origin_of_delivery,                      null: false
+      t.string :deliverying_date,                        null: false
+      t.string :size                                
+      t.integer :buyer_id
+      t.references :user,                                null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_064507) do
+ActiveRecord::Schema.define(version: 2019_05_13_125534) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.string "condition", null: false
+    t.string "status", default: "selling", null: false
+    t.string "who_to_pay", null: false
+    t.string "origin_of_delivery", null: false
+    t.string "deliverying_date", null: false
+    t.string "size"
+    t.integer "buyer_id"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -33,4 +56,5 @@ ActiveRecord::Schema.define(version: 2019_05_07_064507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
