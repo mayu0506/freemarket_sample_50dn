e764a0e747f4f0a8bbbcd85457f7608f2ba08496
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-05-16 14:58:44 +0900

    商品出品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,38 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+    
+      redirect_to @product
+    else
+      render 'new'
+    end
   end
 
   def show
+    
   end
 
   def buy
   end
+
+  private
+
+  def product_params
+    # params.require(:product).permit(:name, :description, :price, :condition, :who_to_pay, :origin_of_delivery, :size, :delivering_date)
+
+    params.require(:product).permit(:name, :description, :price,)
+  end
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,31 @@
 class Product < ApplicationRecord
   has_many :images,dependent: :destroy
   has_many :likes,dependent: :destroy
+
+ 
+
+  # セレクトボックス選択肢の管理
+    enum option: {
+      '---': 0,
+      '新品、未使用': 1,
+      '未使用に近い': 2,
+      '目立った傷や汚れなし': 3,
+      'やや汚れや傷あり': 4,
+      '傷や汚れあり': 5,
+      '全体的に状態が悪い': 6
+    }, _suffix: true
+
+    enum who_to_pay: {
+      '---': 0,
+      '送料込み（出品者負担）': 1,
+      '着払い（購入者負担）': 2
+    }, _suffix: true
+
+    enum delivery_date: {
+      '---': 0,
+      '1~2日で発送': 1,
+      '2~3日で発送': 2,
+      '4~7日で発送': 3,
+    }, _suffix: true
+
 end
