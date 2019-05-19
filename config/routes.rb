74368cf38304b5c15f6dbce08eda22b398a76709
pon182 <pxnxt_182@hotmail.com>
2019-05-19 12:20:15 +0900

    change  comma　potion

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
-  resources :products, only: [:index, :new, :create, :show, :edit ,:destroy] do
+  resources :products, only: [:index, :new, :create, :show, :edit, :destroy] do
     get :buy, on: :member
     get :delete
   end
