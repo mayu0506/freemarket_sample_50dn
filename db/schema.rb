00dccd86734583cf605e0e4eb4ef2f13229b5c73
pon182 <pxnxt_182@hotmail.com>
2019-05-19 15:32:47 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2019_05_17_075324) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
@@ -71,7 +70,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_075324) do
     t.integer "deliverying_date", null: false
     t.string "size"
     t.integer "buyer_id"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
@@ -96,8 +95,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_075324) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "images", "products"
   add_foreign_key "addresses", "users"
+  add_foreign_key "images", "products"
   add_foreign_key "likes", "products"
   add_foreign_key "likes", "users"
   add_foreign_key "payments", "users"
