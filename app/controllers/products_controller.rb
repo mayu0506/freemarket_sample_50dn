954adc853ffae3a5a2ec511d765ab008d1ced6e6
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-05-21 19:11:00 +0900

    画像の登録

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show, :buy, :new]
+  # before_action :authenticate_user!, except: [:index, :show, :buy, :new]
 
 
   def index
@@ -8,19 +8,20 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @image = Image.new
-    @category = Category.all
+    @image = @product.images.build
   
   end
 
   def create
-    @product = Product.new(params_int(product_params))
-    @image = Image.new
+    @product = Product.new(product_params)
 
-
-    binding.pry
+   
 
     if @product.save
+      params[:images]['image'].each do |a|
+        @image = @product.images.create!(image:a)
+      end
+
       redirect_to @product
     else
       render 'new'
@@ -28,7 +29,6 @@ class ProductsController < ApplicationController
   end
 
   def show
-    
   end
 
   def buy
@@ -37,29 +37,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :condition, :who_to_pay, :origin_of_delivery, :size, :deliverying_date, :user_id)
-  #   .merge(user_id: current_user.id)
-  end
-
-  # def image_params
-  #   params.require(:image).permit(:image, :product_id)
-  # end
-
-
-  #フォームに入力されたセレクトボックスの文字列を数値に変換する
-  def integer_string?(str)
-    Integer(str)
-    true
-  rescue ArgumentError
-    false
- end
-
-  def params_int(product_params)
-    product_params.each do |key,value|
-      if integer_string?(value)
-        product_params[key]=value.to_i
-      end
-    end
+    params.require(:product).permit(:name, :description, :price, :condition, :who_to_pay, :origin_of_delivery, :size, :deliverying_date, :user_id, images_attributes: [:image])
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_041529) do
+ActiveRecord::Schema.define(version: 2019_05_16_030625) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,10 +31,10 @@ ActiveRecord::Schema.define(version: 2019_05_16_041529) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "image"
+    t.bigint "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
@@ -60,18 +60,16 @@ ActiveRecord::Schema.define(version: 2019_05_16_041529) do
     t.string "name", default: ""
     t.text "description"
     t.integer "price"
-    t.integer "condition", null: false
+    t.string "condition", null: false
     t.string "status", default: "selling"
-    t.integer "who_to_pay", null: false
-    t.integer "origin_of_delivery", null: false
-    t.integer "deliverying_date", null: false
+    t.string "who_to_pay", null: false
+    t.string "origin_of_delivery", null: false
+    t.string "deliverying_date", null: false
     t.string "size"
     t.integer "buyer_id"
-
     t.bigint "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
