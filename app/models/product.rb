7ce93954258272dc6b63b8cf6d7d6493ba5aff8f
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-05-23 10:58:48 +0900

    配送元のセレクトボックス 実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
+  include JpPrefecture
+  jp_prefecture :origin_of_delivery
+
   has_many :images,dependent: :destroy, inverse_of: :product
   has_many :likes,dependent: :destroy
   belongs_to :category
-
   accepts_nested_attributes_for :images
 end
