c5cab1365b6043196c02a199e7d1b9baff90fedd
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-05-23 12:34:52 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,21 @@ ActiveRecord::Schema.define(version: 2019_05_20_063014) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image"
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "contents"
+    t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image"
+    t.bigint "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
