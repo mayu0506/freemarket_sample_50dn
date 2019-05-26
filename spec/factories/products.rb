9f12f5364dd525d47ebb70c14339995d6c98f2de
pon182 <pxnxt_182@hotmail.com>
2019-05-26 18:35:41 +0900

    update products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,16 +1,18 @@
 FactoryBot.define do
 
   factory :product do
-    id                  {1}
+    id                  {"1"}
     name                {"Deuxieme Classe☆ＯRIVAストライプワイドパンツ"}
     description         {"説明文"}
     price               {"123456"}
-    condition           {1}
+    condition           {"1"}
     status              {"selling"}
-    who_to_pay          {1}
-    origin_of_delivery  {1}
+    who_to_pay          {"1"}
+    origin_of_delivery  {"1"}
+    deliverying_date    {"1"}
     size                {"150"}
-    buyer_id             {"null"}
+    buyer_id            {"null"}
     user_id             {"1"}
+    category_id         {"24"}
   end
 end
