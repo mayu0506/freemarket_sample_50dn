161b657e9c2536c033d6b4021b0c30ddebcba543
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-05-28 21:37:30 +0900

    エラーメッセージ実装

--- app/assets/javascripts/error_message.js
+++ app/assets/javascripts/error_message.js
@@ -1,34 +1,26 @@
-// エラーメッセージ（時間があれば実装）
-// $(function(){
-//   var form = $('#new_product')
-//   var error_message = $('.error')
-//   var submit_button = $('#prodcuct_submit') 
+
+$(function(){
+  var form = $('#new_product')
+  var error_message = $('.error')
+  var submit_button = $('#prodcuct_submit') 
 
 
-//   form.validate({
-//     rules: {
-//       name: {
-//         required: true,
-//         // Using the normalizer to trim the value of the element
-//         // before validating it.
-//         //
-//         // The value of `this` inside the `normalizer` is the corresponding
-//         // DOMElement. In this example, `this` references the `username` element.
-//         normalizer: function(value) {
-//           return $.trim(value);
-//         }
-//       },
-//       description: {required: true}
+  form.validate({
+    rules: {
+      name: {
+        required: true,
+        normalizer: function(value) {
+          return $.trim(value);
+        }
+      },
+      description: {required: true}
 
-//     },
-//     messages: {
-//       name: "入力してください"
-//     },
+    },
+    messages: {
+      name: "入力してください"
+    },
 
 
-//   });
-  
+  });
 
-
-
-// })
+})
