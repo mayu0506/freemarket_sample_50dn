20236e2bdca9d3288cf25a73ccff7d5b81732a3e
mayu0506 <mayu.o@okadaasayuunoMacBook-Pro.local>
2019-05-30 17:00:58 +0900

    S3の修正

--- db/schema.rb
+++ db/schema.rb
@@ -65,26 +65,17 @@ ActiveRecord::Schema.define(version: 2019_05_20_063014) do
     t.integer "price", null: false
     t.integer "condition", null: false
     t.string "status", default: "selling", null: false
-    t.string "who_to_pay", null: false
-    t.string "origin_of_delivery", null: false
-    t.string "deliverying_date", null: false
+    t.integer "who_to_pay", null: false
+    t.integer "origin_of_delivery", null: false
+    t.integer "deliverying_date", null: false
     t.string "size"
     t.integer "buyer_id"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
-  end
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "trades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -106,8 +97,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_063014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "last_name"
     t.string "first_name"
+    t.string "last_name"
     t.string "kana_first_name"
     t.string "kana_last_name"
     t.string "provider"
@@ -123,5 +114,4 @@ ActiveRecord::Schema.define(version: 2019_05_20_063014) do
   add_foreign_key "payments", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
-  add_foreign_key "sns_credentials", "users"
 end
